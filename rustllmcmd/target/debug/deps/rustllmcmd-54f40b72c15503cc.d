8aabcd5601f0fba2e85a6367f0b7469a790b5eba
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-10-26 00:00:02 -0700

    updates

---
+++ rustllmcmd/target/debug/deps/rustllmcmd-54f40b72c15503cc.d
@@ -0,0 +1,5 @@
+/Users/hadrian/Developer/github repos/Rusllm/rustllmcmd/target/debug/deps/librustllmcmd-54f40b72c15503cc.rmeta: src/main.rs
+
+/Users/hadrian/Developer/github repos/Rusllm/rustllmcmd/target/debug/deps/rustllmcmd-54f40b72c15503cc.d: src/main.rs
+
+src/main.rs:
---
+++ rustllmcmd/target/debug/deps/rustllmcmd-bdad3b17cbd776ac.d
@@ -0,0 +1,5 @@
+/Users/hadrian/Developer/github repos/Rusllm/rustllmcmd/target/debug/deps/librustllmcmd-bdad3b17cbd776ac.rmeta: src/main.rs
+
+/Users/hadrian/Developer/github repos/Rusllm/rustllmcmd/target/debug/deps/rustllmcmd-bdad3b17cbd776ac.d: src/main.rs
+
+src/main.rs:
